994439b665b714941e4c994a74ea541c3a1e2cd3
Your Name <arssonist@yahoo.com>
2017-01-27 18:15:54 -0500

    create a set of nested components and run them through a main one;install react-router

--- app/index.js
+++ app/index.js
@@ -1,13 +1,61 @@
+var USER_DATA = {
+  name:'Chris Del',
+  username:'arssonist',
+  pic:'http://www.johndoe.pro/img/John_Doe.jpg'
+}
+
 var React = require('react');
-var reactDOM = require('react-dom');
+var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function(){
-     return(
-      <div>HelloWorld</div>
-     )
-  }
-});  
+    var ProfilePic= React.createClass({
+      render: function(){
+         return(
+          <img src={this.props.ImageUrl} style={{height:100, width:100}}/>
+         )
+      }
+    });
 
-reactDOM.render(<HelloWorld />, document.getElementById('app')
+    var Link = React.createClass({
+      changeURL: function(){
+        // built in DOM method
+      window.location.replace(this.props.href)
+    },
+      render: function(){
+        return(
+          <span style={{color:'blue', cursor:'  pointer'}} onClick={this.changeURL}>
+          {this.props.children}
+          </span>
+        )
+      }
+    });
+
+    var ProfileLink = React.createClass({
+      render: function(){
+         return(
+           <div>
+         <Link href={'https://www.github.com/' +  this.props.username}>
+           {this.props.username}
+        </Link>
+       </div>
+         )
+      }
+    });
+    var ProfileName = React.createClass({
+      render: function(){
+        return <div>{this.props.name}</div>
+      }
+    });
+  var Avatar = React.createClass({
+    render: function(){
+       return(
+        <div>
+          <ProfilePic ImageUrl={this.props.user.pic}/>
+          <ProfileName name={this.props.user.name}/>
+          <ProfileLink username={this.props.user.username}/>
+        </div>
+       )
+    }
+  });
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app')
 );
